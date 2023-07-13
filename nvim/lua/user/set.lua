a3d4e97dd97a9295d5612e7959b0dfcca1510da5
fredy.diaz <fredy.diaz@2degrees.nz>
2023-07-13 15:32:44 +1200

    feat: first commit

---
+++ nvim/lua/user/set.lua
@@ -0,0 +1,21 @@
+vim.opt.nu = true
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.undofile = true
+vim.opt.termguicolors = true
+vim.opt.showcmd = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
+vim.opt.colorcolumn = "90"
+vim.opt.updatetime = 50
+
+vim.opt.isfname:append("@-@")
+
