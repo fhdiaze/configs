8c9372cc9227af2955a2f792c40b525bd023dff6
Fredy Diaz <buzz@DESKTOP-BNESJSM>
2023-08-31 07:05:42 +1200

    feat: change cursor

--- nvim/lua/user/set.lua
+++ nvim/lua/user/set.lua
@@ -1,4 +1,4 @@
-vim.opt.guicursor = ""
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block-iCursor-blinkwait300-blinkon200-blinkoff150,r-cr-o:hor20"
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
